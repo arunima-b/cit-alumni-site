c62b1ae1e841eb222003a25a4c6c2deebc64d02e
Kory Schneider <kory.schneider@gmail.com>
2018-05-24 18:09:36 -0700

    Use ErrorBoundary

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import ErrorBoundary from './ErrorBoundary.jsx';
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    return (<h1>FIXME</h1>);
+    return (
+      <ErrorBoundary>
+        <div id='app'>
+        </div>
+      </ErrorBoundary>
+    );
   }
 }
