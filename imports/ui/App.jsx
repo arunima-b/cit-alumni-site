a3af67738c435529fc8bb5fbf0c81883dedbfe7a
Connor Jones <cjones6@uoregon.edu>
2018-06-01 14:54:38 -0700

    addMenu

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -11,12 +11,44 @@ export default class App extends Component {
   render() {
     return (
       <ErrorBoundary>
-      <Segment>
-        <div id='app'>
-          <h1>hi</h1>
-        </div>
-      </Segment>
-      </ErrorBoundary>
+          <Segment>
+          <Menu>
+
+            <Menu.Item
+              name="menu1"
+              active={true}
+              active={this.state.avtiveItem === 'menu1'}
+              onClick={this.handleMenuItemClick.bind(this)}
+              >Home
+            </Menu.Item>
+
+            <Menu.Item
+              name="menu2"
+              active={true}
+              active={this.state.avtiveItem === 'menu2'}
+              onClick={this.handleMenuItemClick.bind(this)}
+              >News
+            </Menu.Item>
+
+            <Menu.Item
+              name="menu3"
+              active={true}
+              active={this.state.avtiveItem === 'menu3'}
+              onClick={this.handleMenuItemClick.bind(this)}
+              >Courses
+            </Menu.Item>
+
+            <Menu.Item
+              name="menu3"
+              active={true}
+              active={this.state.avtiveItem === 'menu3'}
+              onClick={this.handleMenuItemClick.bind(this)}
+              >People
+            </Menu.Item>
+
+          </Menu>
+          </Segment>
+          </ErrorBoundary>
     );
   }
 }
