908379e0be3208a6b7c8574bc0796144bcde1db2
Kory Schneider <kory.schneider@gmail.com>
2018-06-06 19:09:29 -0700

    Add server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express'),
+  app = express(),
+  path = require('path'),
+  mysql = require('mysql'),
+  dbInfo = require(path.join(__dirname, 'secret', 'db.js'));
+
+//const dbConnection = mysql.createConnection(dbInfo)
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+  //res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.send('hi');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
