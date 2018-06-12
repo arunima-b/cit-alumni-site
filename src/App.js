21303c1568996e476bfa5612a10de2b99f55be21
Kory Schneider <kory.schneider@gmail.com>
2018-06-12 14:27:09 -0700

    Migrate components from meteor project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,75 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Image, Menu, Header, Segment } from 'semantic-ui-react';
+
+import ErrorBoundary from './ErrorBoundary.jsx';
+import Homepage from './Homepage.jsx';
+import Newsfeed from './Newsfeed.jsx';
+import People from './People.jsx';
+
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeItem: 'home'
+    }
+  }
+
+  handleMenuItemClick(e, {name}) {
+    this.setState({ activeItem: name });
+  }
+
   render() {
+    let content;
+    switch(this.state.activeItem) {
+      case 'home':
+        content = <Homepage />;
+        break;
+
+      case 'news':
+        content = <Newsfeed />;
+        break;
+
+      case 'people':
+        content = <People />;
+        break;
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ErrorBoundary>
+        <Segment>
+          <Menu>
+            <Menu.Item
+              name="home"
+              active={true}
+              active={this.state.activeItem === 'home'}
+              onClick={this.handleMenuItemClick.bind(this)}
+              >Home
+            </Menu.Item>
+
+            <Menu.Item
+              name="news"
+              active={true}
+              active={this.state.activeItem === 'news'}
+              onClick={this.handleMenuItemClick.bind(this)}
+              >News
+            </Menu.Item>
+
+            <Menu.Item
+              name="people"
+              active={true}
+              active={this.state.activeItem === 'people'}
+              onClick={this.handleMenuItemClick.bind(this)}
+              >People
+            </Menu.Item>
+          </Menu>
+        </Segment>
+
+        <Segment>
+          {content}
+        </Segment>
+      </ErrorBoundary>
     );
   }
 }
-
-export default App;
---
+++ src/Newsfeed.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Newsfeed extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Newsfeed</h1>
+    )
+  }
+}
