8093fa71437edea3491ec828ea2691cf4d908492
arunima-b <30448199+arunima-b@users.noreply.github.com>
2018-06-13 15:59:07 -0700

    Added header h1

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import React, { Component } from 'react';
-import { Image, Menu, Header, Segment } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { Image, Menu, Header, Segment } from "semantic-ui-react";
 
-import ErrorBoundary from './ErrorBoundary.jsx';
-import Homepage from './Homepage.jsx';
-import Newsfeed from './Newsfeed.jsx';
-import People from './People.jsx';
+import ErrorBoundary from "./ErrorBoundary.jsx";
+import Homepage from "./Homepage.jsx";
+import Newsfeed from "./Newsfeed.jsx";
+import People from "./People.jsx";
 
-import './App.css';
+import "./App.css";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeItem: 'home'
-    }
+      activeItem: "home"
+    };
   }
 
-  handleMenuItemClick(e, {name}) {
+  handleMenuItemClick(e, { name }) {
     this.setState({ activeItem: name });
   }
 
   render() {
     let content;
-    switch(this.state.activeItem) {
-      case 'home':
+    switch (this.state.activeItem) {
+      case "home":
         content = <Homepage />;
         break;
 
-      case 'news':
+      case "news":
         content = <Newsfeed />;
         break;
 
-      case 'people':
+      case "people":
         content = <People />;
         break;
 
@@ -42,37 +42,44 @@ export default class App extends Component {
 
     return (
       <ErrorBoundary>
-        <Segment>
-          <Menu>
-            <Menu.Item
-              name="home"
-              active={true}
-              active={this.state.activeItem === 'home'}
-              onClick={this.handleMenuItemClick.bind(this)}
-              >Home
-            </Menu.Item>
+        <Segment className="header">
+          <div>
+            <Image src="/images/logo/logofinal.jpg" size="small" />
+            <Header as="h1">
+              Computer Information Technology Minor Alumni
+            </Header>
+            <Menu>
+              <Menu.Item
+                name="home"
+                active={true}
+                active={this.state.activeItem === "home"}
+                onClick={this.handleMenuItemClick.bind(this)}
+              >
+                Home
+              </Menu.Item>
 
-            <Menu.Item
-              name="news"
-              active={true}
-              active={this.state.activeItem === 'news'}
-              onClick={this.handleMenuItemClick.bind(this)}
-              >News
-            </Menu.Item>
+              <Menu.Item
+                name="news"
+                active={true}
+                active={this.state.activeItem === "news"}
+                onClick={this.handleMenuItemClick.bind(this)}
+              >
+                News
+              </Menu.Item>
 
-            <Menu.Item
-              name="people"
-              active={true}
-              active={this.state.activeItem === 'people'}
-              onClick={this.handleMenuItemClick.bind(this)}
-              >People
-            </Menu.Item>
-          </Menu>
+              <Menu.Item
+                name="people"
+                active={true}
+                active={this.state.activeItem === "people"}
+                onClick={this.handleMenuItemClick.bind(this)}
+              >
+                People
+              </Menu.Item>
+            </Menu>
+          </div>
         </Segment>
 
-        <Segment>
-          {content}
-        </Segment>
+        <Segment className="content">{content}</Segment>
       </ErrorBoundary>
     );
   }
